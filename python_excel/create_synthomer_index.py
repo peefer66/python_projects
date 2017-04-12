b9e919ac04f5fcfb6593835905718910bbfa0f92
peefer66 <peefer66@hotmail.com>
2017-04-12 14:47:21 +0100

    Modified crreate_synthomer

--- python_excel/create_synthomer_index.py
+++ python_excel/create_synthomer_index.py
@@ -6,20 +6,9 @@ import xlrd
 import csv
 import pandas as pd
 
-def create_row_list(worksheet, row):
-    # This function takes the row and column and appends the value to row list
-    row_list = []
-    # APPEND THE VALUES FOR COMPOUND NUMBER, CUSTOMER & USE AND COVER TO UPPERCASE
-    # BEFORE append
-    row_list.append(worksheet.cell_value(row, 8).upper())
-    row_list.append(worksheet.cell_value(row, 1).upper())
-    # strip the 'USE :' from the start and then lstrip to remove any remaining leading blanks
-    row_list.append(worksheet.cell_value(row,3)[6:len( worksheet.cell_value(row,3).upper())].lstrip(' '))
-    row_list.append(worksheet.cell_value(row + 3, 1))
-    return row_list
 
-if __name__ == ('__main__'):
-    # Itterate through the directory and sub dirs and print ot the file name
+def main():
+	# Itterate through the directory and sub dirs and print ot the file name
     spath = 'H:/Project_Munich_Technology/New_name_ MIs/'
     sheet_list = []
     for  root, dirs, files in os.walk(spath):
@@ -41,3 +30,23 @@ if __name__ == ('__main__'):
     # uSE PANDAS TO CREAT A DATAFRAME FROM  THE SHEET_LIST AND CONVERE TO A CSV FILE.
     my_df = pd.DataFrame(sheet_list)
     my_df.to_csv('output.csv', index=False, header=False)
+
+
+
+def create_row_list(worksheet, row):
+    # This function takes the row and column and appends the value to row list
+    row_list = []
+    # APPEND THE VALUES FOR COMPOUND NUMBER, CUSTOMER & USE AND COVER TO UPPERCASE
+    # BEFORE append
+    row_list.append(worksheet.cell_value(row, 8).upper())
+    row_list.append(worksheet.cell_value(row, 1).upper())
+    # strip the 'USE :' from the start and then lstrip to remove any remaining leading blanks
+    row_list.append(worksheet.cell_value(row,3)[6:len( worksheet.cell_value(row,3).upper())].lstrip(' '))
+    row_list.append(worksheet.cell_value(row + 3, 1))
+    return row_list
+
+	
+if __name__ == ('__main__'):
+	main()
+
+    
